e8feaeb6f9a7e843127d086ffba59dacbd68949a
akash8347 <akashgohil.av@gmail.com>
2025-09-14 15:47:37 +0530

    SMAILL-HeadQuaters update

--- src/app/(policy-pages)/operational-address/page.jsx
+++ src/app/(policy-pages)/operational-address/page.jsx
@@ -3,17 +3,16 @@ import LandingFooter from "@/components/LandingPageComponents/LandingFooter";
 import Navbar from "@/lib/myComponents/Navbar";
 import Link from "next/link";
 export default function RefundPolicy() {
-    return (
-        <>
-        <Navbar/>
-        <div className="max-w-[70%] min-h-[25rem] md:min-h-[30rem] mx-auto  py-8 text-gray-700">
-      <h2 className="text-xl font-semibold mb-2">Operational Address</h2>
-      <p>
-        L.H. Road, Surat, Gujarat, India
-      </p>
-    </div>
-      <LandingFooter/>
-      </>
-    );
-  }
-  
+  return (
+    <>
+      <Navbar />
+      <div className="max-w-[70%] min-h-[20rem] md:min-h-[30rem] mx-auto  py-8 text-gray-700">
+        <h2 className="text-xl font-semibold mb-2">Operational Address</h2>
+        <p>
+          Skill Foundry headquarters is located in Gujarat, India.
+        </p>
+      </div>
+      <LandingFooter />
+    </>
+  );
+}
